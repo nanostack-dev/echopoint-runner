d8fa4ec75871133a24426a81afaca33fb6591a17
Alexis Gardin <gth.shrewdly684@passinbox.com>
2025-10-24 23:53:10 -0400

    refactor: simplify context initialization checks in shared tests

--- it/shared_test.go
+++ it/shared_test.go
@@ -8,15 +8,12 @@ import (
 
 func TestSharedSetup(t *testing.T) {
 	ctx := shared.GetFlowEngineContext()
-	if ctx == nil { //nolint:staticcheck // context initialized in TestMain
-		t.Fatal("test context should be initialized by LaunchTest")
-	}
 
-	if ctx.WireMockURL == "" { //nolint:staticcheck // context is checked above
+	if ctx.WireMockURL == "" {
 		t.Fatal("WireMockURL should be set")
 	}
 
-	t.Logf("WireMock running at: %s", ctx.WireMockURL) //nolint:staticcheck // context is checked above
+	t.Logf("WireMock running at: %s", ctx.WireMockURL)
 }
 
 func TestMain(m *testing.M) {
